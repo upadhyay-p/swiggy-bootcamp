caa11250c4a88ab8d8363042d57f49c68d6e47d6
Priya <priyaupadhyay1.29@gmail.com>
2020-07-22 09:08:50 +0530

    handled invalid choice

--- assignment-1/main.go
+++ assignment-1/main.go
@@ -60,47 +60,51 @@ func main() {
 			//for elem := range emps{
 			//	Print(elem)
 			//}
-			fmt.Println(emps)
+			//fmt.Println(emps)
 			break
 		}
-		var e Emp
-		fmt.Print("Employee ID: ")
-		eid, _ := reader.ReadString('\n')
-		e.EmpId, _ = strconv.ParseInt(strings.TrimSpace(eid), 10, 64)
+		if strings.TrimSpace(choice)=="1" {
+			var e Emp
+			fmt.Print("Employee ID: ")
+			eid, _ := reader.ReadString('\n')
+			e.EmpId, _ = strconv.ParseInt(strings.TrimSpace(eid), 10, 64)
 
-		fmt.Print("\nName: ")
-		name, _ := reader.ReadString('\n')
-		e.Name = strings.TrimSpace(name)
+			fmt.Print("\nName: ")
+			name, _ := reader.ReadString('\n')
+			e.Name = strings.TrimSpace(name)
 
-		fmt.Print("\nSalary: ")
-		sal, _ := reader.ReadString('\n')
-		e.Sal, _ = strconv.ParseInt(strings.TrimSpace(sal), 10, 64)
+			fmt.Print("\nSalary: ")
+			sal, _ := reader.ReadString('\n')
+			e.Sal, _ = strconv.ParseInt(strings.TrimSpace(sal), 10, 64)
 
-		var addr Addr
-		fmt.Print("\nAddress\nHouse No.: ")
-		hno, _ := reader.ReadString('\n')
-		addr.Hno = strings.TrimSpace(hno)
+			var addr Addr
+			fmt.Print("\nAddress\nHouse No.: ")
+			hno, _ := reader.ReadString('\n')
+			addr.Hno = strings.TrimSpace(hno)
 
-		fmt.Print("\nStreet: ")
-		street, _ := reader.ReadString('\n')
-		addr.Street = strings.TrimSpace(street)
+			fmt.Print("\nStreet: ")
+			street, _ := reader.ReadString('\n')
+			addr.Street = strings.TrimSpace(street)
 
-		fmt.Print("\nCity: ")
-		city, _ := reader.ReadString('\n')
-		addr.City = strings.TrimSpace(city)
+			fmt.Print("\nCity: ")
+			city, _ := reader.ReadString('\n')
+			addr.City = strings.TrimSpace(city)
 
-		fmt.Print("\nPin: ")
-		pin, _ := reader.ReadString('\n')
-		addr.Pin = strings.TrimSpace(pin)
+			fmt.Print("\nPin: ")
+			pin, _ := reader.ReadString('\n')
+			addr.Pin = strings.TrimSpace(pin)
 
-		e.Eaddr = addr
+			e.Eaddr = addr
 
-		emps = append(emps, e)
-		fmt.Printf("EmpID: %v, Name: %s, Sal: %v, Address: %s, %s, %s, %s\n\n", e.EmpId, e.Name, e.Sal, e.Eaddr.Hno, e.Eaddr.Street, e.Eaddr.City, e.Eaddr.Pin)
-		fmt.Println(e.Eat())
-		fmt.Println(e.Sleep())
-		fmt.Println(e.Breath())
-		fmt.Println()
+			emps = append(emps, e)
+			fmt.Printf("EmpID: %v, Name: %s, Sal: %v, Address: %s, %s, %s, %s\n\n", e.EmpId, e.Name, e.Sal, e.Eaddr.Hno, e.Eaddr.Street, e.Eaddr.City, e.Eaddr.Pin)
+			fmt.Println(e.Eat())
+			fmt.Println(e.Sleep())
+			fmt.Println(e.Breath())
+			fmt.Println()
+		} else {
+			fmt.Println("Please enter a valid choice.")
+		}
 	}
 
 }
